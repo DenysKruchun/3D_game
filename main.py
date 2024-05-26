e7694c81eb053d2da9b700164cd0824173585c01
DenysKruchun <161941696+DenysKruchun@users.noreply.github.com>
2024-05-26 14:32:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+from ursina.prefabs.sky import Sky
+from ursina.shaders import lit_with_shadows_shader
+
+
+app = Ursina()
+
+Entity.default_shader = lit_with_shadows_shader
+
+
+class Block(Button):
+    def __init__(self, position):
+        super().__init__(parent=scene, model="cube",  
+                         collider='box', texture="grass", 
+                         color=color.hsv(0, 0, random.uniform(.9, 1.0)),
+                          highlight_color=color.rgb(111, 112, 111))
+        self.position = position
+
+    def input (self,key):
+        if self.hovered:
+            if key == "right mouse down":
+                destroy(mouse.hovered_entity)
+            if key == "left mouse down":
+                block = Block(self.position + mouse.normal)
+
+
+for z in range(-8,8):
+    for x in range(-8, 8):
+        block = Block(position=(x,0,z))
+
+# ground = Entity(model="plane", collider='box', texture="grass",
+#                 scale=64, texture_scale=(4, 4))
+sky = Sky(texture="sky_sunset")
+player = FirstPersonController()
+tree = Entity(model = "assets/minecraft_tree/scene", scale =3,  origin_y=0.5)
+app.run()
