1197389eb585bd6bafa0e7235fa6af6d41f813f0
volodymyrlogika <volodymyr.logika@gmail.com>
2024-08-29 16:29:26 +0300

    final_version

--- main.py
+++ main.py
@@ -17,17 +17,35 @@ Entity.default_shader = lit_with_shadows_shader
 
 class Controller(Entity):
     def __init__(self, ):
-        super(). __init__ ()
+        super(). __init__ (ignore_paused = True)
         self.sky = Sky(texture="sky_sunset")
         self.player = FirstPersonController()
        
         # house = Entity(model = "assets/minecraft_house/scene",scale = 1, origin_y = 0, collider   = "box")
         pivot = Entity()
         DirectionalLight(parent=pivot, y=3, z=3, shadows=True,rotation = (45,-45,45))
-        window.fullscreen = True
+        window.fullscreen = True    
         self.menu = Menu(self)
+        mouse.visible = True
+        mouse.locked = False
+        self.toggle_menu()
+        self.start = False
+
+    def toggle_menu(self):
+        application.paused = not application.paused
+        mouse.visible = application.paused
+        mouse.locked = not application.paused
+        self.menu.enabled = application.paused
+
+
 
     def new_game(self, ):
+        self.start  = True
+        for block in Block.list + Tree.list + Chest.list:
+            destroy(block)
+        Block.list = []
+        Chest.list = []
+        Tree.list = []
 
         noise = PerlinNoise(octaves = 4, seed = randint(0,1000))
         for z in range(-MAP_SIZE,MAP_SIZE):
@@ -41,6 +59,7 @@ class Controller(Entity):
                 # chest_ammount = randint(0,700)
                 # if chest_ammount == 25:
                 #     chest = Chest(x,height,z)
+        self.toggle_menu()
     
     def save_game(self, ):
         with open("savings", "wb") as f:
@@ -62,34 +81,45 @@ class Controller(Entity):
             self.save_game()
         elif key == "p":
             self.load_game()
+        elif key == "escape" and self.start:
+            self.toggle_menu()
     
     def load_game(self):
+        self.start  = True
         for block in Block.list + Tree.list + Chest.list:
             destroy(block)
         Block.list = []
         Chest.list = []
         Tree.list = []
-  
-        with open("savings", "rb") as f:
-            self.player.position = pickle.load(f)
-            k = pickle.load(f)
-            for i in range(k):
-                block_pos = pickle.load(f)
-                Block.id = pickle.load(f) 
-                block = Block(position=block_pos)
-            k_tree = pickle.load(f)
-            for i in range (k_tree):
-                tree_position = pickle.load(f)
-                tree_scale = pickle.load(f)
-                tree = Tree(*tree_position)
-                tree.scale = tree_scale
 
-            k_chest = pickle.load(f)   
-            for i in range (k_chest):
-                chest_position = pickle.load(f)
-                chest = Chest(*chest_position)
+        try:
+            with open("savings", "rb") as f:
+                self.player.position = pickle.load(f)
+                k = pickle.load(f)
+                for i in range(k):
+                    block_pos = pickle.load(f)
+                    Block.id = pickle.load(f) 
+                    block = Block(position=block_pos)
+                k_tree = pickle.load(f)
+                for i in range (k_tree):
+                    tree_position = pickle.load(f)
+                    tree_scale = pickle.load(f)
+                    tree = Tree(*tree_position)
+                    tree.scale = tree_scale
+
+                k_chest = pickle.load(f)   
+                for i in range (k_chest):
+                    chest_position = pickle.load(f)
+                    chest = Chest(*chest_position)
+            self.toggle_menu()
+        except:
+            self.new_game()
+        
               
-
+window.exit_button.visible =False
+window.fps_counter.visible = False
+window.entity_counter.visible = False
+window.collider_counter.visible = False
 
 
 # ground = Entity(model="plane", collider='box', texture="grass",
--- ui.py
+++ ui.py
@@ -4,7 +4,7 @@ from ursina import *
 
 class ButtonMenu(Button):
     def __init__(self, text='',  **kwargs):
-        super().__init__(text, scale = (0.8,0.1),texture = "assets/blocks/Andesite_(texture)_JE3_BE2.png", texture_scale = (0.5, 0.5), parent=camera.ui, model= "quad", radius=0.1, origin=(0,0),  text_size=1, color= color.white, collider='box', highlight_scale=1, pressed_scale=1, **kwargs)
+        super().__init__(text, ignore_paused = True,scale = (0.8,0.1),texture = "assets/blocks/Andesite_(texture)_JE3_BE2.png", texture_scale = (0.5, 0.5), model= "quad", radius=0.1, origin=(0,0),  text_size=1, color= color.white, collider='box', highlight_scale=1, pressed_scale=1.05, **kwargs)
 
 
 class Menu(Entity):
@@ -15,5 +15,8 @@ class Menu(Entity):
         self.title = Text(parent = self, text = "Blockcraft",scale = 5,origin=(0,0),x=0,y=0.30,color = color.rgb(0,0,0))
         self.title2 = Text(parent = self, text = "Blockcraft",scale = 4.9,origin=(0,0),x=0.01,y=0.295,color = color.rgb(233,233,233))
 
-        self.button1 = ButtonMenu(text = "Save")
+        self.button1 = ButtonMenu(text = "Save", on_click= game.save_game, y=0.01, parent = self)
+        self.button_2  = ButtonMenu(text = "Load Game",on_click = game.load_game,y = 0.15, parent = self)
+        self.button_3 = ButtonMenu(text= "New Game",on_click = game.new_game,y=-0.13,  parent = self)
+        self.button_4 = ButtonMenu(text = "Escape",on_click  = application.quit,y = -0.27,parent = self)
 
